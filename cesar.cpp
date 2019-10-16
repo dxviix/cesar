9d55fdc1592519685b34b5350cc0e1bd246e1cc7
dxviix <55061508+dxviix@users.noreply.github.com>
2019-10-16 11:03:11 -0300

    Add files via upload

---
+++ cesar.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int numero, i, j, letra;
+	char letra[50];
+	
+	scanf("%d", &numero);
+	
+	for(i=0; numero <= 25; i++) {
+		letra++;
+		scanf("%s", &letra);
+		
+		for(j=0; numer)
+	}
+
+	return 0;
+}
+
