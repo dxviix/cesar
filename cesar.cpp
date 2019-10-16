1ffba3d0509ae2cb81ca86720fbeda61629b1859
dxviix <55061508+dxviix@users.noreply.github.com>
2019-10-16 13:26:04 -0300

    Update cesar.cpp

--- cesar.cpp
+++ cesar.cpp
@@ -2,18 +2,26 @@
 
 int main()
 {
-	int numero, i, j, letra;
-	char letra[50];
+	int numero, i, j, shift;
+	char letra[51];
 	
 	scanf("%d", &numero);
 	
-	for(i=0; numero <= 25; i++) {
-		letra++;
+	for(i=0; i < numero; i++) {
 		scanf("%s", &letra);
+		scanf("%d", &shift);
 		
-		for(j=0; numer)
+		for(j=0; j < 51 && string[j] != '\0'; j++) {
+			if((string[j] - shift) < 65) {
+				string[j] = 91 - (65 - (string[j] - shift));
+			}
+			else {
+				string[j] = string[j] - shift;
+			}
+			printf("%c", string[j]);
+		}
+		printf("\n");	
 	}
-
 	return 0;
 }
 
